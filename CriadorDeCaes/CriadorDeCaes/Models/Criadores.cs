b0b768cd92ad91150a9048de339b0b40ffea5bd5
jcnpereira <casimiro@ipt.pt>
2023-05-08 23:18:51 +0100

    - Criação controller + Views para os 'Criadores' - adição de anotadores ao modelo 'Criadores'

--- CriadorDeCaes/CriadorDeCaes/Models/Criadores.cs
+++ CriadorDeCaes/CriadorDeCaes/Models/Criadores.cs
@@ -1,4 +1,6 @@
-﻿namespace CriadorDeCaes.Models {
+﻿using System.ComponentModel.DataAnnotations;
+
+namespace CriadorDeCaes.Models {
 
    /// <summary>
    /// dados dos criadores dos cães
@@ -10,6 +12,13 @@
       }
 
 
+      /*
+       * Anotadores
+       * https://learn.microsoft.com/en-us/ef/ef6/modeling/code-first/data-annotations
+       * https://www.entityframeworktutorial.net/code-first/dataannotation-in-code-first.aspx
+       */
+
+
       /// <summary>
       /// PK
       /// </summary>
@@ -18,32 +27,40 @@
       /// <summary>
       /// Nome do criador
       /// </summary>
+      [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
       public string Nome { get; set; }
 
       /// <summary>
       /// nome pelo qual o criador é conhecido no 
       /// negócio de venda de cães
       /// </summary>
+      [Display(Name ="Nome Comercial")]
       public string NomeComercial { get; set; }
 
       /// <summary>
       /// Morada do criador
       /// </summary>
+      [Required(ErrorMessage = "A {0} é de preenchimento obrigatório")]
       public string Morada { get; set; }
 
       /// <summary>
       /// Código Postal da morada do criador
       /// </summary>
+      [Required(ErrorMessage ="O {0} é de preenchimento obrigatório")]
+      [Display(Name ="Código Postal")]
       public string CodPostal { get; set; }
 
       /// <summary>
       /// Telemóvel do criador
       /// </summary>
+      [Display(Name ="Telemóvel")]
       public string Telemovel { get; set; }
 
       /// <summary>
       /// Email do criador
       /// </summary>
+      [Required(ErrorMessage = "O {0} é de preenchimento obrigatório")]
+      [EmailAddress]
       public string Email { get; set; }
 
       // *********************************************
